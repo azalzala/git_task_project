b0c39847fb64e5672db4d4e7f8bf51669d079b82
az.worldwide <az.worldwide4science@gmail.com>
2024-04-03 12:29:09 +0100

    fixed code & functionality

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,5 @@
 user_input = input("Begin conversation by typing hello...") 
-if user_input.lower == "hello" or user_input.lower == "hi": 
+if user_input.lower().strip() == "hello" or user_input.lower().strip() == "hi": 
     computer_reply = "Hello, world!"
     print(computer_reply)
 else: 
