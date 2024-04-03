766bc1f4bbfdb8002e23dccee4c879dcd46ccc1c
az.worldwide <az.worldwide4science@gmail.com>
2024-04-03 11:56:26 +0100

    added control logic

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,6 @@
-print("Hello, world!")
+user_input = input("Begin conversation by typing hello...") 
+if user_input.lower == "hello" or user_input.lower == "hi": 
+    computer_reply = "Hello, world!"
+    print(computer_reply)
+else: 
+    print("No entendo")
